b551e21c769a6cf4f09c8129cfca7d84174ba1b7
Eoinburke <burkeyeoin45@gmail.com>
2022-05-17 18:17:27 +0000

    Web scraping added market cap url and printed result

---
+++ web_scraping.py
@@ -0,0 +1,11 @@
+from bs4 import BeautifulSoup
+import requests
+
+url = "https://coinmarketcap.com/"
+result = requests.get(url).text
+doc =   BeautifulSoup(result, "html.parser")
+
+tbody = doc.tbody
+trs = tbody.contents
+
+print(list(trs[0].next_siblings))
