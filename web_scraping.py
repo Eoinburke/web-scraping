c2fee39411cae7191b159a2d656978c60ffbf8e3
Eoinburke <burkeyeoin45@gmail.com>
2022-05-17 18:26:08 +0000

    Find crypto prices

--- web_scraping.py
+++ web_scraping.py
@@ -8,4 +8,13 @@ doc =   BeautifulSoup(result, "html.parser")
 tbody = doc.tbody
 trs = tbody.contents
 
-print(list(trs[0].next_siblings))
+prices = {}
+
+for tr in trs[:10]:
+    name, price =  tr.contents[2:4]
+    fixed_name = name.p.string
+    fixed_price = price.a.string
+
+    prices[fixed_name] = fixed_price
+
+print(prices)
